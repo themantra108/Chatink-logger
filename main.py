0902e27648608a7bfa8f3e3e5581e16510649f67
themantra108 <66516603+themantra108@users.noreply.github.com>
2026-01-29 22:51:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,9 @@ import extract
 import transform
 import load
 
-# --- ORCHESTRATOR ---
 if __name__ == "__main__":
     print("🚀 ETL PIPELINE STARTED")
-    
-    # 1. Run Extract
     raw_data = asyncio.run(extract.get_raw_data())
-    
-    # 2. Run Transform
     clean_data = transform.process_data(raw_data)
-    
-    # 3. Run Load
     load.sync_data(clean_data)
-    
     print("✅ ETL PIPELINE FINISHED")
